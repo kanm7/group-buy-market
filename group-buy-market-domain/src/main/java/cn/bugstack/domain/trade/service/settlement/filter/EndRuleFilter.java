8e8b5864a1d154e2f92c9ae9719a78922a8adf6a
kanm7 <2427646951@qq.com>
2026-04-02 12:06:12 +0800

    feat: 1

--- group-buy-market-domain/src/main/java/cn/bugstack/domain/trade/service/settlement/filter/EndRuleFilter.java
+++ group-buy-market-domain/src/main/java/cn/bugstack/domain/trade/service/settlement/filter/EndRuleFilter.java
@@ -18,22 +18,26 @@ import org.springframework.stereotype.Service;
 public class EndRuleFilter implements ILogicHandler<TradeSettlementRuleCommandEntity, TradeSettlementRuleFilterFactory.DynamicContext, TradeSettlementRuleFilterBackEntity> {
 
     @Override
-    public TradeSettlementRuleFilterBackEntity apply(TradeSettlementRuleCommandEntity requestParameter, TradeSettlementRuleFilterFactory.DynamicContext dynamicContext) throws Exception {
-        log.info("结算规则过滤-结束节点{} outTradeNo:{}", requestParameter.getUserId(), requestParameter.getOutTradeNo());
+    public TradeSettlementRuleFilterBackEntity apply(
+            TradeSettlementRuleCommandEntity requestParameter,
+            TradeSettlementRuleFilterFactory.DynamicContext dynamicContext) throws Exception {
 
-        // 获取上下文对象
+        log.info("结算规则过滤 - 结束节点{} outTradeNo:{}",
+                requestParameter.getUserId(), requestParameter.getOutTradeNo());
+
+        // 从上下文中获取拼团对象
         GroupBuyTeamEntity groupBuyTeamEntity = dynamicContext.getGroupBuyTeamEntity();
 
-        // 返回封装数据
+        // 封装并返回完整信息
         return TradeSettlementRuleFilterBackEntity.builder()
-                .teamId(groupBuyTeamEntity.getTeamId())
-                .activityId(groupBuyTeamEntity.getActivityId())
-                .targetCount(groupBuyTeamEntity.getTargetCount())
-                .completeCount(groupBuyTeamEntity.getCompleteCount())
-                .lockCount(groupBuyTeamEntity.getLockCount())
-                .status(groupBuyTeamEntity.getStatus())
-                .validStartTime(groupBuyTeamEntity.getValidStartTime())
-                .validEndTime(groupBuyTeamEntity.getValidEndTime())
+                .teamId(groupBuyTeamEntity.getTeamId())              // 团队 ID
+                .activityId(groupBuyTeamEntity.getActivityId())      // 活动 ID
+                .targetCount(groupBuyTeamEntity.getTargetCount())    // 目标人数
+                .completeCount(groupBuyTeamEntity.getCompleteCount()) // 已完成人数
+                .lockCount(groupBuyTeamEntity.getLockCount())        // 锁单人数
+                .status(groupBuyTeamEntity.getStatus())              // 拼团状态
+                .validStartTime(groupBuyTeamEntity.getValidStartTime()) // 开始时间
+                .validEndTime(groupBuyTeamEntity.getValidEndTime())     // 结束时间
                 .build();
     }
 
